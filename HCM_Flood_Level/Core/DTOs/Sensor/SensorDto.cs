e07b10b0c2a5649c19639f3f10383c74f1370de0
williamhoang <155402953+toannnse171130@users.noreply.github.com>
2026-01-27 21:12:16 +0700

    add dto

---
+++ HCM_Flood_Level/Core/DTOs/Sensor/SensorDto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs.Sensor
+{
+    public class SensorDto
+    {
+        public int SensorId { get; set; }
+        public string SensorCode { get; set; }
+        public string SensorName { get; set; }
+        public string SensorType { get; set; }
+        public string SensorStatus { get; set; }
+        public DateTime InstalledAt { get; set; }
+        public int LocationId { get; set; }
+        public LocationDto Location { get; set; }
+    }
+
+    public class SensorCreateDto
+    {
+        public string SensorCode { get; set; }
+        public string SensorName { get; set; }
+        public string SensorType { get; set; }
+        public string SensorStatus { get; set; }
+        public DateTime InstalledAt { get; set; }
+        public int LocationId { get; set; }
+    }
+
+    public class SensorUpdateDto
+    {
+        public string SensorName { get; set; }
+        public string SensorType { get; set; }
+        public string SensorStatus { get; set; }
+        public int LocationId { get; set; }
+    }
+
+    public class SensorLocationUpdateDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Address { get; set; }
+    }
+
+    //public class SensorThresholdUpdateDto
+    //{
+    //    public double MinThreshold { get; set; }
+    //    public double MaxThreshold { get; set; }
+    //    public string ThresholdType { get; set; }
+    //}
+
+    public class LocationDto
+    {
+        public int LocationId { get; set; }
+        public string LocationName { get; set; }
+        public string Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int AreaId { get; set; }
+        public string AreaName { get; set; }
+    }
+}
---
+++ HCM_Flood_Level/WebAPI/Models/MappingSensor.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Core.DTOs.Sensor;
+using Core.Entities;
+
+namespace WebAPI.Models
+{
+    public class MappingSensor : Profile
+    {
+        public MappingSensor()
+        {
+            CreateMap<Sensor, SensorDto>()
+                 .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location))
+                 .ForMember(dest => dest.SensorId, opt => opt.MapFrom(src => src.SensorId))
+                 .ForMember(dest => dest.SensorCode, opt => opt.MapFrom(src => src.SensorCode))
+                 .ForMember(dest => dest.SensorName, opt => opt.MapFrom(src => src.SensorName))
+                 .ForMember(dest => dest.SensorType, opt => opt.MapFrom(src => src.SensorType))
+                 .ForMember(dest => dest.SensorStatus, opt => opt.MapFrom(src => src.SensorStatus))
+                 .ForMember(dest => dest.InstalledAt, opt => opt.MapFrom(src => src.InstalledAt))
+                 .ForMember(dest => dest.LocationId, opt => opt.MapFrom(src => src.LocationId));
+                 //.ForMember(dest => dest.MinThreshold, opt => opt.MapFrom(src => src.MinThreshold))
+                 //.ForMember(dest => dest.MaxThreshold, opt => opt.MapFrom(src => src.MaxThreshold))
+                 //.ForMember(dest => dest.ThresholdType, opt => opt.MapFrom(src => src.ThresholdType));
+
+            CreateMap<SensorCreateDto, Sensor>()
+                .ForMember(dest => dest.SensorId, opt => opt.Ignore()) 
+                .ForMember(dest => dest.Location, opt => opt.Ignore()) 
+                .ForMember(dest => dest.SensorCode, opt => opt.MapFrom(src => src.SensorCode))
+                .ForMember(dest => dest.SensorName, opt => opt.MapFrom(src => src.SensorName))
+                .ForMember(dest => dest.SensorType, opt => opt.MapFrom(src => src.SensorType))
+                .ForMember(dest => dest.SensorStatus, opt => opt.MapFrom(src => src.SensorStatus))
+                .ForMember(dest => dest.InstalledAt, opt => opt.MapFrom(src => src.InstalledAt))
+                .ForMember(dest => dest.LocationId, opt => opt.MapFrom(src => src.LocationId));
+
+            CreateMap<SensorUpdateDto, Sensor>()
+                .ForMember(dest => dest.SensorId, opt => opt.Ignore()) 
+                .ForMember(dest => dest.SensorCode, opt => opt.Ignore()) 
+                .ForMember(dest => dest.InstalledAt, opt => opt.Ignore()) 
+                .ForMember(dest => dest.Location, opt => opt.Ignore()) 
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<Location, LocationDto>()
+                .ForMember(dest => dest.LocationId, opt => opt.MapFrom(src => src.LocationId))
+                .ForMember(dest => dest.LocationName, opt => opt.MapFrom(src => src.LocationName))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+                .ForMember(dest => dest.Latitude, opt => opt.MapFrom(src => src.Latitude))
+                .ForMember(dest => dest.Longitude, opt => opt.MapFrom(src => src.Longitude))
+                .ForMember(dest => dest.AreaId, opt => opt.MapFrom(src => src.AreaId))
+                .ForMember(dest => dest.AreaName, opt => opt.MapFrom(src => src.Area != null ? src.Area.AreaName : null));
+
+     
+            CreateMap<Area, AreaDto>()
+                .ForMember(dest => dest.AreaId, opt => opt.MapFrom(src => src.AreaId))
+                .ForMember(dest => dest.AreaName, opt => opt.MapFrom(src => src.AreaName))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
+        }
+    }
+}
