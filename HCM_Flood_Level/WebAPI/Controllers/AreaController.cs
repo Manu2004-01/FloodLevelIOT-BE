b7ee6027709a2727c02f97e3adffe5c99e0e3b96
HuyGiangHo <chegiahuy0640@gmail.com>
2026-04-02 14:48:40 +0700

    Update AreaController.cs

--- HCM_Flood_Level/WebAPI/Controllers/AreaController.cs
+++ HCM_Flood_Level/WebAPI/Controllers/AreaController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Core.DTOs;
+using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,21 +14,24 @@ namespace WebAPI.Controllers
     public class AreaController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public AreaController(IUnitOfWork unitOfWork)
+        public AreaController(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         /// <summary>Lấy tất cả sensor theo mọi khu vực + đọc gần nhất.</summary>
         [HttpGet("areas")]
-        public async Task<ActionResult<IReadOnlyList<AreaDTO>>> GetAll(CancellationToken cancellationToken = default)
+        public async Task<ActionResult> GetAll()
         {
             try
             {
-                var list = await _unitOfWork.AreaRepository.GetAreaSensorReadingsAsync(null, cancellationToken);
-                if (list.Count == 0)
-                    return NotFound(new BaseCommentResponse(404, "Không có dữ liệu khu vực / sensor"));
+                var list = await _unitOfWork.AreaRepository.GetAllAsync();
+                if (list == null)  
+                
+                    return NotFound(new BaseCommentResponse(404, "Không có dữ liệu khu vực"));
 
                 return Ok(list);
             }
